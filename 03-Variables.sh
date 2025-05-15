e971bf68c1c7c8949e9a74a323341f347ee678b6
ravilanka999 <ravi050892@gmail.com>
2025-05-16 06:20:23 +1000

    Variables added

---
+++ 03-Variables.sh
@@ -0,0 +1,6 @@
+PERSON1=$PERSON1
+PERSON2=$PERSON2
+
+echo "$PERSON1:: Hello $PERSON2 how are you?"
+echo "$PERSON2:: Hi $PERSON1 I am fine, what is your plan?"
+echo "$PERSON1:: Nothing, going out for a coffee. Would you like to Join?"
