4e75cf4407fb95e8b94a9f566ec4f7e9ce0cbd97
ravilanka999 <ravi050892@gmail.com>
2025-05-16 06:37:26 +1000

    arguments example

---
+++ 04-Vaiables.sh
@@ -0,0 +1,6 @@
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hello $PERSON2 how are you?"
+echo "$PERSON2:: Hi $PERSON1 I am fine, what is your plan?"
+echo "$PERSON1:: Nothing, going out for a coffee. Would you like to Join?"
