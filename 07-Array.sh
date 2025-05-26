f768ed3fc1e9a2797c0a6a3c64165f607d7fe307
ravilanka999 <ravi050892@gmail.com>
2025-05-26 11:58:15 +1000

    Date variable

--- 07-Array.sh
+++ 07-Array.sh
@@ -3,5 +3,5 @@
 MOVIES=("Kabali" "Badra" "Sankranti")
 
 echo "the movies in an array are ${MOVIES[@]}"
-echo "first movie is ${MOVIES[@0]}"
-echo "Second movie is ${MOVIES[@1]}"
+echo "first movie is ${MOVIES[0]}"
+echo "Second movie is ${MOVIES[1]}"
