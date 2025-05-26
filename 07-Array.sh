4fbc4d440f5a038db7a9ac2e969d54cc4aa729f9
ravilanka999 <ravi050892@gmail.com>
2025-05-26 12:29:58 +1000

    Date variable

--- 07-Array.sh
+++ 07-Array.sh
@@ -5,3 +5,7 @@ MOVIES=("Kabali" "Badra" "Sankranti")
 echo "the movies in an array are ${MOVIES[@]}"
 echo "first movie is ${MOVIES[0]}"
 echo "Second movie is ${MOVIES[1]}"
+
+echo "which user is running the script $USER"
+echo "Present working directory $PWd"
+echo "the script that is being executed is $0"
