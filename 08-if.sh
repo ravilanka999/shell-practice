775caf10261048dc5a62fce92d6cf94086aa0327
ravilanka999 <ravi050892@gmail.com>
2025-05-26 13:45:21 +1000

    Date variable

--- 08-if.sh
+++ 08-if.sh
@@ -2,13 +2,13 @@
 
 NUMBER=$1
 
-if [ $NUMBER -lt 10 ] {
+if [ $NUMBER -lt 10 ]
 then 
     echo " Given numbe $NUMBER is less than 10"
-}
-else{
+
+else
     echo "Given number $NUMBER is greater than 10"
-}
+
 fi
 
 
