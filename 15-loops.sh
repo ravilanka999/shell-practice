184700d56f0d9c985b98db17059414ee235ecedc
ravilanka999 <ravi050892@gmail.com>
2025-05-27 21:09:41 +1000

    Date variable

--- 15-loops.sh
+++ 15-loops.sh
@@ -8,6 +8,7 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
 PACKAGES=("mysql" "nginx" "python" "httpd") 
 
 mkdir -p $LOGS_FOLDER
